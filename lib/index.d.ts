aeb63110d7d9e26f3f0409e59ce9531acf7f4040
selemondev <selemondev19@gmail.com>
2022-09-30 08:38:26 -0700

    add: float

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,16 +3,23 @@
  * @param max The maximum number to include in returned generated integer.
  * @param exclude (optional) Number/integer to exclude.
  */
-declare const integer: (max: number, exclude?: number | undefined) => number;
+declare const integer: (max: number, exclude?: number) => number;
 /**
  * Returns a random generated character.
  * @param length The number of characters to include in returned generated string.
  * @param excludeNumbers (Optional) To exclude numbers in returned generated string.
  */
-declare const char: (length: number, excludeNumbers?: boolean | undefined) => string;
+declare const char: (length: number, excludeNumbers?: boolean) => string;
 /**
  * Returns a random generated emoji.
  * @param excludeEmoji (optional) Emoji to exclude.
 */
-declare const emoji: (excludeEmoji?: string | undefined) => string;
-export { integer, char, emoji };
+declare const emoji: (excludeEmoji?: string) => string;
+/**
+ * Returns a random generated integer/number.
+ * @param max The maximum number to include in returned generated float.
+ * @param exclude (optional) Number/integer to exclude.
+ * @param decimals (optional) Format a number using fixed-point notation.
+ */
+declare const float: (max: number, exclude?: number, decimals?: number) => number;
+export { integer, char, emoji, float };
